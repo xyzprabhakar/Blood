5bcd39b9b461708cd749f009766ea070be96934c
Sumit2705 <33717521+Sumit2705@users.noreply.github.com>
2018-03-06 11:57:58 +0530

    Update MainActivity.java

--- app/src/main/java/com/glazegalway/bloodstain/MainActivity.java
+++ app/src/main/java/com/glazegalway/bloodstain/MainActivity.java
@@ -4,10 +4,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    String st_name = "Sumit"
+    TextView tv_textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        tv_textview = (TextView) findviewbyid(R.id.tv_textview);
+        tv_textview.setText(st_name);
+          
+        
+        
     }
 }
